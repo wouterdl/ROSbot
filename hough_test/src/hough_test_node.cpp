6221a3bdade2063fad05f7bb4d0555bffb4b2502
wouterdl <45484347+wouterdl@users.noreply.github.com>
2019-05-02 14:02:58 +0200

    Add files via upload

--- hough_test/src/hough_test_node.cpp
+++ hough_test/src/hough_test_node.cpp
@@ -1,42 +1,51 @@
+
 #include <ros/ros.h>
+#include "opencv2/opencv.hpp"
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <vector>
-#include "opencv2/opencv.hpp"
+#include <opencv2/core/core.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <std_msgs/Int64MultiArray.h>
-#include <message_filters/subscriber.h>
-#include <message_filters/time_synchronizer.h>
 
 
-class Line2image
+class Linedata
 {
 
 public:
 
-  cv::Mat imagelines;
-  void imagecallback(const sensor_msgs::ImageConstPtr& image, const std_msgs::Int64MultiArray::ConstPtr& coords);
-
-};
-
-void Line2image::imagecallback(const sensor_msgs::ImageConstPtr& image, const std_msgs::Int64MultiArray::ConstPtr& coords)
-{
-
-  cv_bridge::CvImagePtr imageCV;
-
-
-
+  int lines[8];
   cv::Point point1;
   cv::Point point2;
   cv::Point point3;
   cv::Point point4;
+  void linescallback(const std_msgs::Int64MultiArray::ConstPtr& coords);
+};
 
-  //point1.x = coords[0];
-  //point1.y = coords[4];
+void Linedata::linescallback(const std_msgs::Int64MultiArray::ConstPtr& coords)
+{
+  int array[8] = {coords->data[0], coords->data[1], coords->data[2], coords->data[3], coords->data[4], coords->data[5], coords->data[6], coords->data[7]};
+  cv::Mat Points(1, 8, CV_32SC1, array);
+  point1 = {Points.at<int>(0,0), Points.at<int>(0,1)};
+  point2 = {Points.at<int>(0,2), Points.at<int>(0,3)};
+  point3 = {Points.at<int>(0,4), Points.at<int>(0,5)};
+  point4 = {Points.at<int>(0,6), Points.at<int>(0,7)};
+}
 
+class Imagedata
+{
 
+public:
+
+  cv_bridge::CvImagePtr imageCV;
+  cv::Mat imageCV2;
+  void imagecallback(const sensor_msgs::ImageConstPtr& image);
+};
+
+void Imagedata::imagecallback(const sensor_msgs::ImageConstPtr& image)
+{
   try
     {
     imageCV = cv_bridge::toCvCopy(image, sensor_msgs::image_encodings::BGR8); //convert ros image format to cv image
@@ -46,10 +55,7 @@ void Line2image::imagecallback(const sensor_msgs::ImageConstPtr& image, const st
     ROS_ERROR("cv_bridge exeption %s", e.what());
     return;
     }
-
-    imagelines = cv::Mat::ones(3,3, CV_8UC3);
-  //imagelines = cv::line(cv::line(imageCV, point1, point2, cv::Scalar(0,255,0), 1, FILLED), point3, point4, cv::Scalar(0,255,0), 1, FILLED);
-
+    imageCV2 = imageCV->image;
 
 }
 
@@ -58,27 +64,29 @@ int main(int argc, char **argv)
 
   ros::init(argc, argv, "hough_test");
   ros::NodeHandle n;
-  Line2image line2image;
+  Linedata linedata;
+  Imagedata imagedata;
 
-  //Subscribing to 2 topics and sync
-  message_filters::Subscriber<sensor_msgs::Image> image_sub(n, "/videofile/image_raw", 1);
-  message_filters::Subscriber<std_msgs::Int64MultiArray> coords_sub(n, "Hough", 1);
-  message_filters::TimeSynchronizer<sensor_msgs::Image, std_msgs::Int64MultiArray> sync(image_sub, coords_sub, 10);
-  sync.registerCallback(boost::bind(&Line2image::imagecallback, _1, _2));
+  image_transport::ImageTransport it(n);
+  image_transport::Subscriber imgsub;
+  imgsub = it.subscribe("/videofile/image_raw", 1, &Imagedata::imagecallback, &imagedata);
 
-  ros::Rate loop_rate(3);
+  ros::Subscriber linesub = n.subscribe("Hough", 10, &Linedata::linescallback, &linedata);
+  image_transport::Publisher imgpub = it.advertise("image_lines", 1);
 
-  image_transport::Imagetransport it(n);
-  image_transport::Publisher pub = it.advertise("image_lanes", 1);
-  sensor_msgs::ImagePtr imgmsg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", line2image.imagelines).toImageMsg();
-
+  ros::Rate loop_rate(20);
 
   while (ros::ok())
   {
-    pub.publish(imgmsg);
+    cv::line(imagedata.imageCV2, linedata.point1, linedata.point2, cv::Scalar(0,255,0), 3, -1);
+    cv::line(imagedata.imageCV2, linedata.point3, linedata.point4, cv::Scalar(0,255,0), 3, -1);
+    sensor_msgs::ImagePtr imgmsg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", imagedata.imageCV2).toImageMsg();
+
+    imgpub.publish(imgmsg);
     ros::spinOnce();
     loop_rate.sleep();
   }
+
 
 
 }
